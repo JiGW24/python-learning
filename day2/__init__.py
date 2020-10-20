7bb2dc0af427b23eda2652df813063001838e2dd
JiGW24 <634579807@qq.com>
2020-10-20 22:33:33 +0800

    函数的定义

---
+++ day2/__init__.py
@@ -0,0 +1,14 @@
+# def getNum(num):
+#     if num == 2:
+#         return -2;
+#     else:
+#         return 0;
+#
+# print(getNum(0))
+# print(getNum(2))
+
+print('hello',end='')
+print('world')
+
+print('a','b','c')
+print('a','b','c',sep='*')
